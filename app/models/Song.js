7f364202f51bca3d66f3060aa1aefb7db3a0f68f
Steven Laurino <sjlaurino@gmail.com>
2019-02-17 18:01:25 -0700

    fixed card growing issue. audio not working

--- app/models/Song.js
+++ app/models/Song.js
@@ -12,13 +12,13 @@ export default class Song {
     getTemplate(i) {
         return `
     <div class="carousel-item ${!i ? 'active' : ''}">
-        <div class=" card d-flex flex-column align-items-center justify-content-center w-100 wrap-text">        
+        <div class="card d-flex flex-column align-items-center justify-content-center w-100">        
             <img class="card-img-top" src="${this.albumArt}" alt="First slide">
-            <h5 class="card-title">${this.title}</h5>
-            <p class="card-text">${this.artist}</p>
+           <div class="wrap-text"> <h5 class="card-title">${this.title}</h5></div>
+            <div class="wrap-text"> <p class="card-text">${this.artist}</p></div>
             <audio controls src="${this.preview}">
-                    Your browser does not support the
-                    <code>audio</code> element.
+                    <p>Your browser does not support the
+                    <code>audio</code> element.</p>
             </audio>
             <p class="card-text"><small class="text-muted">${this.price}</small></p>
         </div>
